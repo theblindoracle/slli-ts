69774981c6917ecc444f55112a3a6882bd695d83
Travis Napier <travisnapier@Traviss-MacBook-Pro.local>
2024-01-10 19:57:05 -0500

    disable middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {LiftingcastModule} from './liftingcast/liftingcast.module';
-import {EventEmitterModule} from '@nestjs/event-emitter';
-import {SlliModule} from './slli/slli.module';
-import {SingularliveModule} from './singularlive/singularlive.module';
-import {LoggerMiddleware} from './liftingcast/common/middleware/logger.middleware';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LiftingcastModule } from './liftingcast/liftingcast.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { SlliModule } from './slli/slli.module';
+import { SingularliveModule } from './singularlive/singularlive.module';
+import { LoggerMiddleware } from './liftingcast/common/middleware/logger.middleware';
 
 @Module({
   imports: [
@@ -19,6 +19,6 @@ import {LoggerMiddleware} from './liftingcast/common/middleware/logger.middlewar
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    // consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
