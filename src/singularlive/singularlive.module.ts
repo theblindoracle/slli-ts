fb7dad627d19446ea9fa296a91c91e9215314746
Travis Napier <travisnapier@Traviss-MacBook-Pro.local>
2024-01-21 16:17:41 -0500

    remove unneeded singularlive files

--- src/singularlive/singularlive.module.ts
+++ src/singularlive/singularlive.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
 import { SingularliveService } from './singularlive.service';
 import { HttpModule } from '@nestjs/axios';
-import { SingularliveController } from './singularlive.controller';
 import { LiftingcastModule } from 'src/liftingcast/liftingcast.module';
 import { LiftingcastService } from 'src/liftingcast/liftingcast.service';
 import { LiftingcastEndpoint } from 'src/liftingcast/liftingcast.endpoint';
+import { MainScene } from './scenes/singularlive.mainscene';
+import { SceneManagerService } from './singularlive.scenemanager';
 
 @Module({
   imports: [HttpModule, LiftingcastModule],
-  providers: [SingularliveService, LiftingcastService, LiftingcastEndpoint],
-  controllers: [SingularliveController],
+  providers: [
+    SingularliveService,
+    LiftingcastService,
+    LiftingcastEndpoint,
+    MainScene,
+    SceneManagerService,
+  ],
+  controllers: [],
 })
 export class SingularliveModule {}
