9202bfb131ade130d3ff87e5291387936ae23751
Travis Napier <travisnapier@Traviss-MacBook-Pro.local>
2024-02-20 16:48:10 -0500

    implement fuzzy find for ranking

--- src/rankings/rankings.model.ts
+++ src/rankings/rankings.model.ts
@@ -1,118 +1,54 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { RankingsService } from './rankings.service';
-import { GetRankingsDTO, UsaplService } from 'src/usapl/usapl.service';
+import Fuse from 'fuse.js';
+import { LiftingcastDivisionDecoder } from 'src/liftingcast/liftingcast.decoder';
 import {
-  DivisionOptions,
-  EquipmentLevelOptions,
-  WeightClassOptions,
-} from 'src/usapl/usapl.dtos';
-import { RankingDTO } from './rankings.dto';
+  slliToUsaplEquipmentMap,
+  slliToUsaplGenderMap,
+} from 'src/slli/premeet/premeet.service';
+import { Ranking } from './rankings.entity';
 
 @Injectable()
 export class RankingsModel {
   private readonly logger = new Logger(RankingsModel.name);
-  constructor(
-    private readonly rankingsService: RankingsService,
-    private readonly usaplService: UsaplService,
-  ) {}
+  constructor(private readonly rankingsService: RankingsService) {}
 
-  async getRankings() {
-    const getRankingsDTOs = new Array<GetRankingsDTO>();
+  async getLifterRanking(
+    lcName: string,
+    lcWeightClass: string,
+    lcDivisionName: string,
+  ): Promise<Ranking | null> {
+    const divisionDetails = new LiftingcastDivisionDecoder().decode(
+      lcDivisionName,
+    );
 
-    const equipmentLevels: Array<EquipmentLevelOptions> = ['raw', 'equipped'];
+    const equipment = slliToUsaplEquipmentMap.get(
+      divisionDetails.equipmentLevel,
+    );
 
-    const maleWeightClasses: Array<WeightClassOptions> = [
-      '-30',
-      '-35',
-      '-40',
-      '-44',
-      '-48',
-      '-52',
-      '-56',
-      '-60',
-      '-67.5',
-      '-75',
-      '-82.5',
-      '-90',
-      '-100',
-      '-110',
-      '-125',
-      '-140',
-      '140+',
-    ];
+    const gender = slliToUsaplGenderMap.get(divisionDetails.gender);
 
-    const femaleWeightClasses: Array<WeightClassOptions> = [
-      '-30',
-      '-35',
-      '-40',
-      '-44',
-      '-48',
-      '-52',
-      '-56',
-      '-60',
-      '-67.5',
-      '-75',
-      '-82.5',
-      '-90',
-      '-100',
-      '100+',
-    ];
+    const rankings = await this.rankingsService.findBy({
+      weightClassID: lcWeightClass,
 
-    for (const equipmentLevel of equipmentLevels) {
-      for (const weightClass of maleWeightClasses) {
-        getRankingsDTOs.push({
-          equipmentLevel: equipmentLevel,
-          weightClass: weightClass,
-          division: DivisionOptions.Open,
-          orderBy: 'points_dots',
-          discipline: 'total',
-          sex: 'm',
-        });
-      }
+      equipmentLevel: equipment,
+      sex: gender,
+    });
 
-      for (const weightClass of femaleWeightClasses) {
-        getRankingsDTOs.push({
-          equipmentLevel: equipmentLevel,
-          weightClass: weightClass,
-          division: DivisionOptions.Open,
-          orderBy: 'points_dots',
-          discipline: 'total',
-          sex: 'f',
-        });
-      }
+    const fuse = new Fuse(rankings, {
+      keys: ['name'],
+      isCaseSensitive: false,
+      includeScore: true,
+    });
+
+    const searchResults = fuse.search(lcName);
+
+    this.logger.log(searchResults);
+
+    if (searchResults.length > 0) {
+      return searchResults[0].item;
     }
 
-    for (const getRankingsDTO of getRankingsDTOs) {
-      this.logger.log('Getting Rankings from USAPL DB');
-      this.logger.log(getRankingsDTO);
-
-      const usaplRankings = await this.usaplService.getRanking(getRankingsDTO);
-
-      this.logger.log(usaplRankings);
-
-      const rankingDTOs = new Array<RankingDTO>();
-
-      for (const ranking of usaplRankings) {
-        const dto: RankingDTO = {
-          name: `${ranking.lifter.firstName} ${ranking.lifter.lastName}`,
-          position: ranking.position,
-          points: ranking.points,
-          sex: ranking.lifter.sex,
-          equipmentLevel: getRankingsDTO.equipmentLevel,
-          discipline: getRankingsDTO.discipline,
-          division: getRankingsDTO.division,
-          weightClassID: getRankingsDTO.weightClass.replace('-', ''),
-        };
-
-        rankingDTOs.push(dto);
-      }
-
-      for (const dto of rankingDTOs) {
-        await this.rankingsService.create(dto);
-      }
-    }
-    //get ranking from usapl db
-
-    return await this.rankingsService.findAll();
+    return null;
   }
 }
