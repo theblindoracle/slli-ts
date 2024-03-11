8d442e0bee254031c86b59ebf810f60a0261cf57
Travis Napier <travisnapier@Traviss-MacBook-Pro.local>
2024-03-11 19:47:02 -0400

    add top 5 rankings and decrease fuze threshold

--- src/rankings/rankings.controller.ts
+++ src/rankings/rankings.controller.ts
@@ -1,7 +1,21 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { RankingsService } from './rankings.service';
 import { RankingsModel } from './rankings.model';
 
 @Controller('rankings')
 export class RankingsController {
-  constructor(private readonly rankingsModel: RankingsModel) {}
+  constructor(
+    private readonly rankingsModel: RankingsModel,
+    private readonly rankingsService: RankingsService,
+  ) { }
+
+  @Get()
+  async get() {
+    return await this.rankingsService.findAll();
+  }
+
+  @Get('topFive')
+  async getTopFive() {
+    return await this.rankingsModel.getTopFiveRankings();
+  }
 }
--- src/rankings/rankings.model.ts
+++ src/rankings/rankings.model.ts
@@ -39,17 +39,82 @@ export class RankingsModel {
       keys: ['name'],
       isCaseSensitive: false,
       includeScore: true,
+      threshold: 0.15,
     });
 
     const searchResults = fuse.search(lcName).sort((a, b) => b.score - a.score);
-
+    this.logger.debug('rankings search results', lcName, searchResults);
     if (searchResults.length > 0) {
-      if (searchResults[0].score > 0.75) {
-        this.logger.log(searchResults[0]);
-        return searchResults[0].item;
-      }
+      return searchResults[0].item;
     }
 
     return null;
+  }
+
+  async getTopFiveRankings() {
+    const equipmentLevels: Array<string> = ['raw', 'equipped'];
+    const maleWeightClasses: Array<string> = [
+      '30',
+      '35',
+      '40',
+      '44',
+      '48',
+      '52',
+      '56',
+      '60',
+      '67.5',
+      '75',
+      '82.5',
+      '90',
+      '100',
+      '110',
+      '125',
+      '140',
+      '140+',
+    ];
+
+    const femaleWeightClasses: Array<string> = [
+      '30',
+      '35',
+      '40',
+      '44',
+      '48',
+      '52',
+      '56',
+      '60',
+      '67.5',
+      '75',
+      '82.5',
+      '90',
+      '100',
+      '100+',
+    ];
+    const rankings = [];
+    for (const equipmentLevel of equipmentLevels) {
+      for (const weightClass of maleWeightClasses) {
+        const mensRankings = await this.rankingsService.findBy({
+          sex: 'm',
+          weightClassID: weightClass,
+          equipmentLevel: equipmentLevel,
+        });
+        const sortedAndSliced = mensRankings
+          .sort((a, b) => a.position - b.position)
+          .slice(0, 5);
+        rankings.push(...sortedAndSliced);
+      }
+
+      for (const weightClass of femaleWeightClasses) {
+        const womensRankings = await this.rankingsService.findBy({
+          sex: 'f',
+          weightClassID: weightClass,
+          equipmentLevel: equipmentLevel,
+        });
+        const sortedAndSliced = womensRankings
+          .sort((a, b) => a.position - b.position)
+          .slice(0, 5);
+        rankings.push(...sortedAndSliced);
+      }
+    }
+    return rankings;
   }
 }
